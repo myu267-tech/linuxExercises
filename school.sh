620e772a213ca76ad5037b18f178739f02f4c04e
Menghan Yu <myu267@wisc.edu>
2026-03-03 09:06:17 -0600

    Implement school.sh

--- school.sh
+++ school.sh
@@ -1,1 +1,21 @@
-#!/bin/bash 
+#!/bin/bash
+
+cat Property_Tax_Roll.csv \
+  | grep "MADISON SCHOOLS" \
+  | cut -d',' -f7 \
+  | {
+      sum=0
+      count=0
+
+      while read -r x
+      do
+        sum=$((sum + x))
+        count=$((count + 1))
+      done
+
+      if [ "$count" -gt 0 ]; then
+        echo $((sum / count))
+      else
+        echo 0
+      fi
+    }
