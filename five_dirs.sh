7c73a02bdd84a4f16ce59f4e697f3a78fd6cef81
Siqi Qiu <your email address>
2026-03-05 09:29:22 -0600

    Add code to five_dirs.sh to create five directories and files

--- five_dirs.sh
+++ five_dirs.sh
@@ -1,1 +1,13 @@
 #!/bin/bash
+mkdir -p five
+
+for i in {1..5}; do
+  dir="five/dir$i"
+  mkdir -p "$dir"
+  for j in {1..4}; do
+    for k in $(seq 1 $j); do
+      echo "$j" >> "$dir/file$j"
+    done
+  done
+done
+
